82e8717d4f172b9f571dac38a3aad9d5b2629011
GitBitWheat <67285289+GitBitWheat@users.noreply.github.com>
2020-06-25 23:49:40 +0300

    Now on gVim (:

--- Vector.cpp
+++ Vector.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "Hello World!\n";
+template<int SIZE> class Vector {
+private:
+	double vals[SIZE];
+public:
+	Vector<SIZE>& operator+= (Vector<SIZE> other) {
+		for(int i = 0; i < SIZE; i++) {
+			vals[i] += other.vals[i];
+		}
+		return *this;
+	}
+
+	Vector<SIZE>& operator-= (Vector<SIZE> other) {
+		for(int i = 0; i < SIZE; i++) {
+			vals[i] -= other.vals[i];
+		}
+		return *this;
+	}
+
+	double& operator[] (int index) {
+		return vals[index];
+	}
+	
+	int size() {
+		return SIZE;
+	}
+};
+
+template<int SIZE> Vector<SIZE> operator+ (Vector<SIZE> v1, Vector<SIZE> v2) {
+	Vector<SIZE> result = v1;
+	result += v2;
+	return result;
+}
+
+template<int SIZE> Vector<SIZE> operator- (Vector<SIZE> v1, Vector<SIZE> v2) {
+	Vector<SIZE> result = v1;
+	result -= v2;
+	return result;
 }
