3f035fec2f0d08c3dc98436ba760b1f878035984
GitBitWheat <67285289+GitBitWheat@users.noreply.github.com>
2020-06-26 09:14:12 +0300

    Added VectorDouble and multiplication by scalars.

--- Vector.cpp
+++ Vector.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template<int SIZE> class Vector {
 private:
-	double vals[SIZE];
+	class VectorDouble { 
+	private:
+		double val;
+	public:
+		operator double() {
+			return val;
+		}
+
+		void operator= (double d) {
+			val = d;
+		}
+	};
+
+	VectorDouble vals[SIZE];
 public:
 	Vector() {
 		for (int i = 0; i < SIZE; i++) {
@@ -13,19 +26,19 @@ public:
 
 	Vector<SIZE>& operator+= (Vector<SIZE> other) {
 		for(int i = 0; i < SIZE; i++) {
-			vals[i] += other.vals[i];
+			vals[i] = vals[i] + other.vals[i];
 		}
 		return *this;
 	}
 
 	Vector<SIZE>& operator-= (Vector<SIZE> other) {
 		for(int i = 0; i < SIZE; i++) {
-			vals[i] -= other.vals[i];
+			vals[i] = vals[i] - other.vals[i];
 		}
 		return *this;
 	}
 
-	double& operator[] (int index) {
+	VectorDouble& operator[] (int index) {
 		return vals[index];
 	}
 	
@@ -44,6 +57,18 @@ template<int SIZE> Vector<SIZE> operator- (Vector<SIZE> v1, Vector<SIZE> v2) {
 	Vector<SIZE> result = v1;
 	result -= v2;
 	return result;
+}
+
+template<int SIZE> Vector<SIZE> operator* (double scalar, Vector<SIZE> v) {
+	Vector<SIZE> result = v;
+	for (int i = 0; i < SIZE; i++) {
+		result[i] = result[i] * scalar;
+	}
+	return result;
+}
+
+template<int SIZE> Vector<SIZE> operator* (Vector<SIZE> v, double scalar) {
+	return scalar * v;
 }
 
 //for convinience
--- VectorTest.cpp
+++ VectorTest.cpp
@@ -7,4 +7,15 @@ int main() {
 	vec3[1] = 20;
 	cout << vec3[2] << endl;
 	cout << vec3[0] <<", "<< vec3[1] <<", "<< vec3[2] << endl;
+
+	//The following lines create an error, so the VectorDouble class probably class works
+	/*
+	double& x = vec3[0];
+	x = 10;
+	cout << x << endl;
+	*/
+
+	//Checking if multipication by scalar works
+	vec3 = 2 * vec3; 
+	cout << vec3[0] <<", "<< vec3[1] <<", "<< vec3[2] << endl;
 }
